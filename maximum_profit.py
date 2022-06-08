33be06ab0ab82a2a1a7d9fcddc5f8e3ee67da247
siddharthsundaram <98947917+siddharthsundaram@users.noreply.github.com>
2022-06-07 20:46:11 -0500

    Update maximum_profit.py

--- maximum_profit.py
+++ maximum_profit.py
@@ -1,23 +1,16 @@
-#  File: maximum_profit.py
-#  Description:
-#  Student Name: Siddharth Sundaram
-#  Student UT EID: svs833
-#  Partner Name: Lauren Adams
-#  Partner UT EID: la27334
-#  Course Name: CS 313E
-#  Unique Number: 51130
-#  Date Created: 04/23/2022
-#  Date Last Modified: 04/24/2022
-
 import sys
 
-# Add Your functions here
+# dynamic programming knapsack type problem
 def maximizeProfit(money, prices, increase):
+    
     # create 2d array dimensions money x # houses
     arr = [[0 for i in range(money + 1)] for j in range(len(prices) + 1)]
     for i in range(1, len(prices) + 1):
         for j in range(money + 1):
-            if prices[i - 1] <= j:  # while any houses can be afforded
+            
+            # while any houses can be afforded
+            if prices[i - 1] <= j:
+                
                 # find max between value in row above and value in row above, column-weight
                 arr[i][j] = max(arr[i - 1][j], (increase[i - 1] * prices[i - 1]) + arr[i - 1][j - prices[i - 1]])
             else:
@@ -25,10 +18,6 @@ def maximizeProfit(money, prices, increase):
                 arr[i][j] = arr[i - 1][j]
 
     return "%0.2f" % (arr[len(prices)][money])
-
-# You are allowed to change the main function. 
-
-# Do not change the template file name. 
 
 def main():
 
@@ -61,11 +50,8 @@ def main():
         increase[i] = float(increase[i])/100
 
 
-
-# Add your implementation here .... 
     result = maximizeProfit(money, prices, increase)
 
-# Add your functions and call them to generate the result. 
 
     print(result)
 
